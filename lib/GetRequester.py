fbff0fa2698eadee177f6d0897b6f159cc756656
Chris Lozzi <clozzi12@gmail.com>
2024-01-18 12:26:44 -0500

    complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data_list = []
+        data = json.loads(self.get_response_body())
+        for item in data:
+            data_list.append(item)
+
+        return data_list
